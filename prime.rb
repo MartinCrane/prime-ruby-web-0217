7b0e6988e3df6a91ec3c3692938f8e2ffdd99bc0
Martin Crane <martin.crane@gmail.com>
2017-01-20 17:23:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  else
+    multiples = (2...number).to_a
+    prime = true
+    multiples.each do |factor|
+      if number % factor == 0
+        prime = false
+      else
+      end
+    end
+    prime
+  end
+end
